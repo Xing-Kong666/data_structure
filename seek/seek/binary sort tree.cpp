00137e470783773054a05db50dbec0cd5bfb9871
Xing-Kong666 <3156142863@qq.com>
2026-08-18 20:47:49 +0800

    修改了binary sort tree的存在野指针导致无法正常修改二叉排序树的若干问题

--- seek/seek/binary sort tree.cpp
+++ seek/seek/binary sort tree.cpp
@@ -17,7 +17,7 @@ typedef struct {
 /*二叉排序树节点类型*/
 typedef struct bst {
 	elemtype key;/*查找关键字结构体*/
-	bst* lchild=NULL, * rchild=NULL;/*树的左右孩子*/
+	bst* lchild, * rchild;/*树的左右孩子*/
 }bst,*bstree;/*查找表和根节点*/
 
 
@@ -36,17 +36,16 @@ bstree Binary_Sort_Tree_Search(bstree b, keytype key) {
 
 /*初始化节点*/
 /*开辟空间并给数据域赋值，返回空间地址*/
-bstree Init_Binary_Sort_Tree(bstree active,int goal) {
-	active = (bst*)malloc(sizeof(bst));
-	if (!active)return NULL;
+void Init_Binary_Sort_Tree(bstree &active,int goal) {
+	active = (bst*)calloc(1,sizeof(bst));
+	if (!active)return;
 	active->key.word = goal;
-	return active;
 }/*Init_Binary_Sort_Tree*/
 
 /*二叉排序树的插入*/
-int Binary_Sort_Tree_Insert(bstree b, keytype key) {
+int Binary_Sort_Tree_Insert(bstree &b, keytype key) {
 	if (!b) {
-		b = (bst*)malloc(sizeof(bst));
+		b = (bst*)calloc(1,sizeof(bst));
 		if (!b)return -1;
 		b->key.word = key;
 		return ok;
@@ -54,13 +53,13 @@ int Binary_Sort_Tree_Insert(bstree b, keytype key) {
 	else if (key > b->key.word)
 		Binary_Sort_Tree_Insert(b->rchild, key);
 	else if (key < b->key.word)
-		Binary_Sort_Tree_Insert(b->rchild, key);
+		Binary_Sort_Tree_Insert(b->lchild, key);
 	else return ok;
 }/*Binary_Sort_Tree_Insert*/
 
 /*二叉排序树的生成*/
-int Binary_Sort_Tree_Init(bstree b, int* keys,int keyslength) {
-	b = (bst*)malloc(sizeof(bst));
+int Binary_Sort_Tree_Init(bstree &b, int* keys,int keyslength) {
+	b = (bst*)calloc(1,sizeof(bst));
 	if (!b)return -1;
 	bstree root = b;
 	b->key.word = keys[0];
@@ -69,7 +68,7 @@ int Binary_Sort_Tree_Init(bstree b, int* keys,int keyslength) {
 			if (keys[i] < b->key.word) {/*小*/
 				if (b->lchild) b = b->lchild;/*后面有就继续比较*/
 				else {/*后面没有就开辟空间填入结束*/
-					b->lchild = (bst*)malloc(sizeof(bst));
+					b->lchild = (bst*)calloc(1,sizeof(bst));
 					b = b->lchild;
 					b->key.word = keys[i];
 					break;
@@ -78,7 +77,7 @@ int Binary_Sort_Tree_Init(bstree b, int* keys,int keyslength) {
 			else if (keys[i] > b->key.word) {/*大*/
 				if (b->rchild) b = b->rchild;/*后面有继续比较*/
 				else {/*没有就填入*/
-					b->rchild = (bst*)malloc(sizeof(bst));
+					b->rchild = (bst*)calloc(1,sizeof(bst));
 					b = b->rchild;
 					b->key.word = keys[i];
 					break;
@@ -115,32 +114,36 @@ int main() {
 	if (!quite)return -1;
 	/*赋值*/
 	quite->key.word = 122;/*左子树赋值*/
-	active->lchild = Init_Binary_Sort_Tree(active->lchild,99);
+	Init_Binary_Sort_Tree(active->lchild,99);
 	active = active->lchild;
-	active->rchild = Init_Binary_Sort_Tree(active->rchild,110);
+	Init_Binary_Sort_Tree(active->rchild,110);
 	active = active->rchild;
-	active->lchild = Init_Binary_Sort_Tree(active->lchild, 105);
+	Init_Binary_Sort_Tree(active->lchild, 105);
 	active = active->lchild;
 	active = quite;/*回到根节点*/
-	active->rchild = Init_Binary_Sort_Tree(active->rchild, 250);/*右子树赋值*/
+	Init_Binary_Sort_Tree(active->rchild, 250);/*右子树赋值*/
 	active = active->rchild;
-	active->lchild = Init_Binary_Sort_Tree(active->lchild, 200);
-	active->rchild = Init_Binary_Sort_Tree(active->rchild, 300);
+	Init_Binary_Sort_Tree(active->lchild, 200);
+	Init_Binary_Sort_Tree(active->rchild, 300);
+	bstree active2 = active->rchild;
 	active = active->lchild;
-	active->rchild = Init_Binary_Sort_Tree(active->rchild, 230);
+	Init_Binary_Sort_Tree(active->rchild, 230);
 	active = active->rchild;
-	active->lchild = Init_Binary_Sort_Tree(active->lchild, 216);
+	Init_Binary_Sort_Tree(active->lchild, 216);
+	active = active->lchild;
 	active = quite;/*回到根节点*/
 	/*算法查找*/
 	keytype key1 = 105;/*目标值*/
-	bstree end = Binary_Sort_Tree_Search(quite, key1);
+	bstree end = Binary_Sort_Tree_Search(active, key1);
 	if (!end)printf("没找着");
 	else printf("找到了，地址是%p,内容是%d\n", end,end->key.word);
+	printf("显示查找的二叉排序树: ");
 	Print_Tree(active);
 	printf("\n");
 	/*算法插入*/
 	keytype key2 = 100;
 	Binary_Sort_Tree_Insert(active, key2);
+	printf("插入100: ");
 	Print_Tree(active);
 	printf("\n");
 	/*释放当前额外内存*/
@@ -149,12 +152,14 @@ int main() {
 	quite = NULL;
 	/*算法生成*/
 	keytype keys[7] = { 55,11,23,42,25,256,253 };
-	bstree b = (bst*)malloc(sizeof(bst));
+	bstree b = (bst*)calloc(1,sizeof(bst));
 	if (!b)return -1;
 	Binary_Sort_Tree_Init(b, keys, 7);
+	printf("生成二叉排序树: ");
 	Print_Tree(b);
 	/*释放内存*/
 	Free_Tree(b);
+	b = NULL;
 
 	return 0;
 }

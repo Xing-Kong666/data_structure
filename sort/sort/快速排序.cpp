69036d7bf363aa5cfc88cffd25b296c93fccb358
Xing-Kong666 <3156142863@qq.com>
2026-09-02 16:27:48 +0800

    对sort->快速排序进行了注释补充

--- sort/sort/快速排序.cpp
+++ sort/sort/快速排序.cpp
@@ -1,7 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-/*分区排序，每一个枢轴的左右大小归类*/
+/*对每一个枢轴左右进行划分*/
 int Partition(int* arr, int left, int right) {
 	arr[0] = arr[left];
 	int pivot = arr[left];
@@ -15,6 +15,13 @@ int Partition(int* arr, int left, int right) {
 	return left;
 }
 /*快速排序，对每一个枢轴排序，直到只剩枢轴自身为止*/
+/*最小时间复杂度:o(n*logn)
+最大时间复杂度:o(n*n)
+平均时间复杂度:o(n*logn)
+最小空间复杂度:o(logn)
+最大空间复杂度:o(n)
+平均空间复杂度:o(logn)
+不稳定排序*/
 void Quick_Sort(int* arr, int left,int right) {
 	if (left < right) {
 		int pivot = Partition(arr, left, right);
